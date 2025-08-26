48d9146eacd9e4796d6090dd7e610e3bf9a91b8d
Ryan Yappert <ryappert@gmail.com>
2025-08-25 21:18:16 -0700

    Fix Postgres migration... again.

--- Arrowgene.Ddon.Database/Files/Database/Script/migration_equipment_limit_break_datatype.sql
+++ Arrowgene.Ddon.Database/Files/Database/Script/migration_equipment_limit_break_datatype.sql
@@ -4,8 +4,8 @@ CREATE TABLE "temp_limit_break"
     "item_uid"         TEXT       NOT NULL,
     "effect_id"        INTEGER    NOT NULL,
     "unk1"             INTEGER    NOT NULL,
-    "effect_type"      TINYINT    NOT NULL,
-    "unk0"             TINYINT    NOT NULL
+    "effect_type"      SMALLINT    NOT NULL,
+    "unk0"             SMALLINT    NOT NULL
 );
 
 INSERT INTO "temp_limit_break" SELECT 
@@ -13,8 +13,8 @@ INSERT INTO "temp_limit_break" SELECT
     "item_uid",
     "effect_1",
     "effect_2",
-    CAST("is_effect1_valid" AS TINYINT),
-    CAST("is_effect2_valid" AS TINYINT)
+    1,
+    0
 FROM "ddon_equipment_limit_break";
 
 DROP TABLE "ddon_equipment_limit_break";
@@ -26,8 +26,8 @@ CREATE TABLE "ddon_equipment_limit_break"
     "item_uid"         TEXT       NOT NULL,
     "effect_id"        INTEGER    NOT NULL,
     "unk1"             INTEGER    NOT NULL,
-    "effect_type"      TINYINT    NOT NULL,
-    "unk0"             TINYINT    NOT NULL,
+    "effect_type"      SMALLINT    NOT NULL,
+    "unk0"             SMALLINT    NOT NULL,
     CONSTRAINT "pk_ddon_equipment_limit_break" PRIMARY KEY ("character_id", "item_uid", "effect_type"),
     CONSTRAINT "fk_ddon_equipment_limit_break_item_uid" FOREIGN KEY ("item_uid") REFERENCES "ddon_storage_item" ("item_uid") ON DELETE CASCADE,
     CONSTRAINT "fk_ddon_equipment_limit_break_character_id" FOREIGN KEY ("character_id") REFERENCES "ddon_character" ("character_id") ON DELETE CASCADE
--- Arrowgene.Ddon.Database/Files/Database/Script/schema_sqlite.sql
+++ Arrowgene.Ddon.Database/Files/Database/Script/schema_sqlite.sql
@@ -927,8 +927,8 @@ CREATE TABLE IF NOT EXISTS "ddon_equipment_limit_break"
     "item_uid"         TEXT       NOT NULL,
     "effect_id"        INTEGER    NOT NULL,
     "unk1"             INTEGER    NOT NULL,
-    "effect_type"      TINYINT    NOT NULL,
-    "unk0"             TINYINT    NOT NULL,
+    "effect_type"      SMALLINT    NOT NULL,
+    "unk0"             SMALLINT    NOT NULL,
     CONSTRAINT "pk_ddon_equipment_limit_break" PRIMARY KEY ("character_id", "item_uid", "effect_type"),
     CONSTRAINT "fk_ddon_equipment_limit_break_item_uid" FOREIGN KEY ("item_uid") REFERENCES "ddon_storage_item" ("item_uid") ON DELETE CASCADE,
     CONSTRAINT "fk_ddon_equipment_limit_break_character_id" FOREIGN KEY ("character_id") REFERENCES "ddon_character" ("character_id") ON DELETE CASCADE
